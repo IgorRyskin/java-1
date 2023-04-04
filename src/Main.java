14de60a838253e8177fe476738a3d9bc684f28ee
Tiger <i.riskin@yandex.ru>
2023-04-04 11:42:54 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int replenishment = 300;
+        int bonus = replenishment < 1000 ? replenishment / 100 : replenishment;
+        int finish = balance + replenishment + bonus;
+        System.out.println("Итоговый баланс" + finish);
+
+    }
+}
